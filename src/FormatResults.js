c5b54016bfa24cbc978f201f79d9e61de5ebd533
Sam Kelleher <sam@samkelleher.com>
2017-02-08 23:41:26 +0000

    Begin colour output the table for easier testing.

--- src/FormatResults.js
+++ src/FormatResults.js
@@ -1,4 +1,5 @@
 import Table from 'cli-table';
+import colors from 'colors/safe';
 
 export default function FormatResults(results) {
     const table = new Table({
@@ -16,8 +17,8 @@ export default function FormatResults(results) {
         library.libraryName,
         `${library.width} x ${library.height}`,
         library.device,
-        library.colorSpaceName,
-        library.colorProfile,
+        colors.red(library.colorSpaceName),
+        library.colorProfile === 'No Profile' ? colors.red(library.colorProfile) : library.colorProfile,
         `${(library.count[0] * 1e9) + library.count[1]} (${library.count[0]}s ${library.count[1] / 1000000}ms)`
     ];
 
